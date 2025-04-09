8f1fb84332c14e8f6c1e395594f7ba8c19ca7824
mhmood2008 <mahmodeyounes2008@gmail.com>
2025-04-09 20:15:55 +0300

    Added word occurrence count and order within the surah

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 import pandas as pd
 import re
-import os
 
 app = Flask(__name__)
 
@@ -21,16 +20,28 @@ def search():
         lambda x: bool(re.search(rf'\b{re.escape(query)}\b', str(x), flags=re.IGNORECASE))
     )]
 
+    # إحصاء التكرار في القرآن والسورة
+    sura_counts = {}  # قاموس لحفظ عدد التكرار في السورة
+    total_count = 0  # عدد التكرار في القرآن
     results = []
+
     for _, row in matching_rows.iterrows():
+        sura_name = row['sura_name_ar']
+        
+        # تحديث عدد التكرار في السورة
+        if sura_name not in sura_counts:
+            sura_counts[sura_name] = 0
+        sura_counts[sura_name] += 1
+        total_count += 1
+        
+        # إضافة النتيجة مع رقم التكرار في السورة
         results.append({
-            'رقم': row['id'],
-            'سورة': row['sura_name_ar'],
+            'رقم': sura_counts[sura_name],  # رقم التكرار في السورة
+            'سورة': sura_name,
             'آية': row['aya_text_emlaey']
         })
 
-    return render_template('results.html', query=query, results=results)
+    return render_template('results.html', query=query, results=results, total_count=total_count)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
