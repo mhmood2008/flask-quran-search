b8507aaaca299fecda7cab1bc828524dda13beca
mhmood2008 <mahmodeyounes2008@gmail.com>
2025-04-09 20:50:12 +0300

    Added word occah

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import pandas as pd
 import re
+import os
 
 app = Flask(__name__)
 
@@ -44,4 +45,5 @@ def search():
     return render_template('results.html', query=query, results=results, total_count=total_count)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
